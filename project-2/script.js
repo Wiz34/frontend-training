c466ee08392df59c25d2415551fc578fac7479ab
Dwayne T <dect55@gmail.com>
2025-04-10 22:51:41 -0400

    add input validatoin and result styling to converter

--- project-2/script.js
+++ project-2/script.js
@@ -1,8 +1,16 @@
 function convertToBTC() {
     const usd = document.getElementById('usd-input'). value;
+    const result = document.getElementById('btc-result')
+    if  ( usd ==="" || usd <= 0) {
+        result.textContent = "Please enter a valid USD amount."; 
+        result.style.color = 'red';
+        return;
+    }
+   
     const btcPrice = 80000;
     const btc = usd / btcPrice;
-    document.getElementById('btc-result').textContent = `BTC: ${btc.toFixed(8)}`;
+   result.textContent = `BTC: ${btc.toFixed(8)}`;
+   result.style.color = "#f7931a"
     console.log('usd')
     //Timestamp
     const now = new Date().toLocaleTimeString();
@@ -13,5 +21,7 @@ function convertToBTC() {
 function clearConverter() {
     document.getElementById('usd-input').value = ''
     document.getElementById("btc-result").textContent = 'BTC: 0';
+    const now = new Date().toLocaleTimeString();
+    document.getElementById('last-updated').textContent = `Last updated: ${now}`;
     console.log('clear')
 }
